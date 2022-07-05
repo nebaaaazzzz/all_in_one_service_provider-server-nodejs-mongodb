f00e5e14a1293b12efd3cb5c4dbcf43186648bd2
nebiyu <nebiyuf@gmail.com>
2022-07-05 14:56:00 +0300

    posthouse completed

--- routes/lesser/index.js
+++ routes/lesser/index.js
@@ -1,19 +1,26 @@
 const upload = require("../../config/fileHandler");
 
 const route = require("express").Router();
-// const cloudinary = require("cloudinary").v2;
-
-route.post("/posthouse", upload.array("houseimage", 5), async (req, res) => {
-  res.send("hello wolr");
-  //   cloudinary.uploader.upload("", {})(
-  //     "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  //     { public_id: "olympic_flag" },
-  //     function (error, result) {
-  //       console.log(result);
-  //     }
-  //   );
-});
-route.get("/house" ,async ( req, res)=>{
-    
-})
+const House = require("./../../models/House");
+route.post(
+  "/posthouse",
+  (req, res, next) => {
+    console.log("uploading");
+    next();
+  },
+  upload.array("houseImage", 100),
+  async (req, res) => {
+    const obj = JSON.parse(req.body.body);
+    obj.houseImages = req.files.map((file) => file.id);
+    obj.price = Number(obj.price);
+    obj.location = { type: "Point", coordinates: obj.center };
+    delete obj.center;
+    await House.create({ ...obj, user: req.user.id });
+    res.status(201).send({
+      success: true,
+    });
+  }
+);
+route.get("/house:id", async (req, res) => {});
+route.get("/posts", async (req, res) => {});
 module.exports = route;
